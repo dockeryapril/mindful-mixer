fd22aea309ae37b3a6ae61a5e0886b49509a0e9c
April Dockery <dockeryapril@gmail.com>
2025-02-19 11:25:11 -0700

    testing new code in index.tsx after more troubleshooting (TS)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function App() {
+export default function IndexScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hello, Mindful Mixer!</Text>
